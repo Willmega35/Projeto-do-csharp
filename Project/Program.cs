8678a6c731507dc7073b8d9dddb26db1b790012b
Willard <Will.Dev2022@gmail.com>
2023-04-23 20:37:26 -0300

    Esse é codigo do meu De rolagem de dados

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Rolar de Dados: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)){
+    if((roll1 == roll2) && (roll2 == roll3)){
+        Console.WriteLine("Você tirou triplo acerto! Sera adcionado  mais +6 pontos!");
+        total += 6;
+    }
+
+    else{
+        Console.WriteLine("Você tirou duplo acerto! Serão adcionado mais +3 pontos!");
+        total += 3;
+    }
+}
+
+if (total >= 15){
+    Console.WriteLine("Voc~e venceu!!");
+}
+
+else if (total <= 14) {
+    Console.WriteLine("Você conseguiu um carro!!");
+}
+
+else if (total > 10) {
+    Console.WriteLine("Você consegui um Notbook Game!!");
+}
+
+else if (total == 8) {
+    Console.WriteLine("Você conseguir uma viagem só  de ida para qualquer lugar!")
+}
+
+else if (total <= 7) {
+    Console.WriteLine("Você consegui um aperto de mão")
+}
